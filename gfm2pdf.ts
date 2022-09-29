486645fcb8b55a4a30af6ca6854652fe81b50d19
Will Dixon <will@willd.io>
2022-09-29 01:56:50 -0400

    Add PostCSS support and Highlight JS for Code Blocks

--- gfm2pdf.ts
+++ gfm2pdf.ts
@@ -1,29 +1,47 @@
+// Markdown -> HTML
 import { unified } from "https://esm.sh/unified@10.1.2";
 import remarkParse from "https://esm.sh/remark-parse@10.0.1";
 import remarkGfm from "https://esm.sh/remark-gfm@3.0.1";
 import remarkRehype from "https://esm.sh/remark-rehype@10.1.0";
 import rehypeStringify from "https://esm.sh/rehype-stringify@9.0.3";
 import rehypeDocument from "https://esm.sh/rehype-document@6.1.0";
+import rehypeHighlight from "https://esm.sh/rehype-highlight@5.0.2";
 import puppeteer from "https://deno.land/x/puppeteer@16.2.0/mod.ts";
-import { toFileUrl } from "https://deno.land/std@0.157.0/path/mod.ts";
+
+// Postcss
+import postcss from "https://deno.land/x/postcss@8.4.16/mod.js";
+import { AcceptedPlugin } from "https://deno.land/x/postcss@8.4.16/lib/postcss.d.ts";
+import postcssImport from "https://esm.sh/postcss-import@15.0.0";
+import postcssImportUrl from "https://esm.sh/postcss-import-url@7.0.0";
+import postcssNested from "https://esm.sh/postcss-nested@5.0.6";
+import postcssMixins from "https://esm.sh/postcss-mixins@9.0.4";
+import postcssPresetEnv from "https://esm.sh/postcss-preset-env@7.8.2";
+
+//VFile
 import { read, toVFile, write } from "https://esm.sh/to-vfile@7.2.3";
 import { VFile } from "https://esm.sh/vfile@5.3.5";
+
+// STD
+import { toFileUrl } from "https://deno.land/std@0.157.0/path/mod.ts";
 import { dirname, resolve } from "https://deno.land/std@0.157.0/path/mod.ts";
 import { parse } from "https://deno.land/std@0.157.0/flags/mod.ts";
 
 type Flags = {
   [x: string]: unknown;
   help: boolean;
+  styles?: string;
   _: string[];
 };
 
 type Options = {
   inFile: VFile;
   outFile: string;
+  styles?: VFile;
 };
 
 type ProcessMarkdownOptions = {
   title?: string;
+  style?: string;
 };
 
 type ProcessHtmlToPDFOptions = {
@@ -34,7 +52,8 @@ await main();
 
 async function main() {
   const opts = await handleArgs();
-  const htmlFile = await processMarkdownToHtml(opts.inFile);
+  const styles = await processCss(opts.styles);
+  const htmlFile = await processMarkdownToHtml(opts.inFile, { style: styles });
   await processHtmlToPDF(htmlFile, {
     output: opts.outFile,
   });
@@ -43,8 +62,10 @@ async function main() {
 async function handleArgs(): Promise<Options> {
   const flags = parse(Deno.args, {
     boolean: ["help"],
+    string: ["styles"],
     alias: {
       "help": ["h"],
+      "styles": ["s"],
     },
   }) as Flags;
 
@@ -64,10 +85,23 @@ async function handleArgs(): Promise<Options> {
     outFile = resolve(flags._[1]);
   }
   Deno.mkdir(dirname(outFile), { recursive: true });
-  return {
+
+  const opts: Options = {
     inFile,
     outFile,
   };
+  if (flags.styles) {
+    opts.styles = await read(toVFile(await Deno.realPath(flags.styles)));
+  } else {
+    try {
+      opts.styles = await read(
+        toVFile(await Deno.realPath("./styles/index.css")),
+      );
+    } catch {
+      console.debug("No Styles were found");
+    }
+  }
+  return opts;
 }
 
 function printHelp() {
@@ -81,6 +115,21 @@ function printHelp() {
   console.log("");
   console.log("OPTIONS:");
   console.log("--help, -h     Print out this help message.");
+  console.log("--styles, -s   Location of your root CSS file.");
+}
+
+async function processCss(file?: VFile): Promise<string | undefined> {
+  if (!file) {
+    return undefined;
+  }
+  const results = await postcss()
+    .use(postcssImport as AcceptedPlugin)
+    .use(postcssImportUrl as AcceptedPlugin)
+    .use(postcssMixins as AcceptedPlugin)
+    .use(postcssNested as AcceptedPlugin)
+    .use(postcssPresetEnv as AcceptedPlugin)
+    .process(file.value, { from: file.path });
+  return results.css;
 }
 
 async function processMarkdownToHtml(
@@ -95,7 +144,12 @@ async function processMarkdownToHtml(
     .use(remarkParse)
     .use(remarkGfm)
     .use(remarkRehype)
-    .use(rehypeDocument, { title: opts.title })
+    .use(rehypeHighlight, {
+      subset: false,
+      ignoreMissing: true,
+      plainText: ["no-highlight"],
+    })
+    .use(rehypeDocument, { title: opts.title, style: opts.style })
     .use(rehypeStringify)
     .process(file);
 
@@ -123,6 +177,6 @@ async function processHtmlToPDF(
   await page.pdf({
     path: opts.output,
   });
-  browser.close();
+  await browser.close();
   return read(toVFile(opts.output));
 }
